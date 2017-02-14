2ae393f5c05c009f0d2017b90987bb357b301da4
Ella <erj503@gmail.com>
2017-02-14 21:16:53 +0000

    Added up to exercise 5

---
+++ ex4.rb
@@ -0,0 +1,21 @@
+# Variables and Names
+
+cars = 100
+
+# space_in_a_car uses floating point so calculation can be performed
+# but is restricted by finite precision issues
+
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
